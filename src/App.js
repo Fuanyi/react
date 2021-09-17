1cae58407b0ee8a9968cc0ad53ed21e942b73ee5
Judech <judefuanyi@gmail.com>
2021-09-17 15:05:26 -0700

    Added COntact Manager Text

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import react from 'react';
+import Header from './Components/Header'
+import AddContact from './Components/AddContact'
+import ContactList from './Components/ContactList'
+import './App.css';
+// import Greet from './Components/Greet';
+// import Welcome from './Components/Welcome';
+function App() {
+  return (
+    <div className="App">
+     <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
